71b780f9a4429740af68611bd72c99f2013d0bce
Nathan Liska <liska.nathan@gmail.com>
2021-09-03 02:58:01 -0400

    Initial commit
    Main Arduino file

---
+++ main/main.ino
@@ -0,0 +1,22 @@
+/*
+ * OER_RECOAT
+ * Version 0.1
+ */
+
+/*
+ * Libraries
+ */
+#include <ESP8266WiFi.h>
+
+
+// Setup
+void setup() {
+  Serial.begin(115200);
+  Serial.println("NodeMCU starting...")
+  
+}
+
+// Main Loop
+void loop() {
+  
+}
